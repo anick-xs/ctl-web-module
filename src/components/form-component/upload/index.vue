2556ba9f1bb48c2cbaaf066a1f55bb8303cb6e5a
吴 信双 <wuxinshuang@chelizitech.com>
2019-04-23 19:49:47 +0800

    前端组件v0.0.2

---
+++ src/components/form-component/upload/index.vue
@@ -0,0 +1,37 @@
+/**
+* User: wuxinshuang
+* Date: 2019/4/23
+* Time: 19:38
+* remark:
+*/
+<template>
+    <el-form-item :label="model.name" :key="model.key" :prop="model.key" :rules="model.rules" >
+       <!-- <upload v-model="formData[model.key]"
+                :formData="formData"
+                :path="model.path"
+                :type="model.type"
+                :limit="model.limit"
+                :zip="model.zip"
+                :fileName="model.fileName"
+                :only="model.only"
+                :coverUrl.sync="model.coverUrl"
+                @uploadOnChange="event => onChange(event, model, index,formData,formModel)"
+                :disabledIs="formModel.disabled || model.disabled || disabled">
+        </upload>-->
+    </el-form-item>
+</template>
+
+<script>
+    export default {
+        name: "index",
+        props:{
+            formData:[Object],
+            model:[Object],
+            formModel:[Object],
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
